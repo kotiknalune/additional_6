c4406d255dc5699b9d8e9b3c0c1ece5ba85e763b
Ekaterina Raylyan <31958491+kotiknalune@users.noreply.github.com>
2017-09-29 18:09:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 module.exports = function zeros(expression) {
-var getDigitsForMultiplication = function(number, exclamationCount) {
-	var digits = [];
-	while(number > 0) {
-		digits.push(number);
-		number = number - exclamationCount;
+
+	var getDigitsForMultiplication = function(number, exclamationCount) {
+		var digits = [];
+		while(number > 0) {
+			digits.push(number);
+			number = number - exclamationCount;
+		}
+		return digits.reverse(); // to match the order in task, rly doesnt mttr
 	}
-	return digits.reverse(); // to match the order in task, rly doesnt mttr
-}
 
-var getExclamationCount = function(string) {
-	var substrings = string.split("!");
-	return substrings.length - 1;
-}
-
-var multiplyNumbersInArray = function(numbers) {
-	var result = 1;
-	for (var i = 0; i < numbers.length; i++) {
-		result = result * numbers[i]
+	var getExclamationCount = function(string) {
+		var substrings = string.split("!");
+		return substrings.length - 1;
 	}
-	return result;
-}
 
-var getMultiplicationResults = function(string) {
-	var result = [], numbersWithExclMarks = string.split('*');
-	for (var i = 0; i < numbersWithExclMarks.length; i++) {
-		var numberString = numbersWithExclMarks[i],
-		exclamationCount = getExclamationCount(numberString),
-		digitsToMultiply = getDigitsForMultiplication(parseInt(numberString), exclamationCount),
-		multipliedDigits = multiplyNumbersInArray(digitsToMultiply);
-		result.push(multipliedDigits);
+	var multiplyNumbersInArray = function(numbers) {
+		var result = 1;
+		for (var i = 0; i < numbers.length; i++) {
+			result = result * numbers[i]
+		}
+		return result;
 	}
-	return result;
-}
 
-var zeroes = function(string) {
-	var separateNumbersEvaluationResults = getMultiplicationResults(string),
+	var getMultiplicationResults = function(string) {
+		var result = [], numbersWithExclMarks = string.split('*');
+		for (var i = 0; i < numbersWithExclMarks.length; i++) {
+			var numberString = numbersWithExclMarks[i],
+			exclamationCount = getExclamationCount(numberString),
+			digitsToMultiply = getDigitsForMultiplication(parseInt(numberString), exclamationCount),
+			multipliedDigits = multiplyNumbersInArray(digitsToMultiply);
+			result.push(multipliedDigits);
+		}
+		return result;
+	}
+
+	var separateNumbersEvaluationResults = getMultiplicationResults(expression),
 	stringEvaluationResult = multiplyNumbersInArray(separateNumbersEvaluationResults),
 	numberOfZeroesAtTheEnd = 0;
 
